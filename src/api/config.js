261fe7fb1738301b85fc9250e62317e8123a265f
misha4322 <mishnefnara@gmail.com>
2025-07-01 04:14:48 +0300

    Merge remote changes

--- src/api/config.js
+++ src/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3001';
+export const BASE_URL = 'https://ps-server-production.up.railway.app';
 
 export const API_ENDPOINTS = {
   AUTH: {
